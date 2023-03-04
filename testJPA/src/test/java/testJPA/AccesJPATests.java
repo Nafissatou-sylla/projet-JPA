e72110906a573b58396fc8a6434c67c6dbc09692
Sylla <finasylla2020@gmail.com>
2023-03-04 21:51:22 +0100

    ajout des dependances

--- testJPA/src/test/java/testJPA/AccesJPATests.java
+++ testJPA/src/test/java/testJPA/AccesJPATests.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 import org.formation.sylla.testJPA.service.PersonnePOJO;
 import org.formation.sylla.testJPA.stockage.DaoJPA;
-import org.junit.Test;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,14 +14,14 @@ class AccesJPATests {
 	
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
-	jpa = new DaoJPA();
+		jpa = new DaoJPA();
 	}
 	
 	@Test
 	void testLireTous() {
 	List<PersonnePOJO> res = null;
 	res = jpa.lireTous();
-	assertEquals(3, res.size());
+	assertEquals(2, res.size());
 	}
 
 
