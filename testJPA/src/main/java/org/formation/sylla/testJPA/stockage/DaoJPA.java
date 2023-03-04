54c322cbc66570c9a102264d09d0ca82bf8c55b7
Sylla <finasylla2020@gmail.com>
2023-03-04 17:52:32 +0100

    classe DaoJPA

---
+++ testJPA/src/main/java/org/formation/sylla/testJPA/stockage/DaoJPA.java
@@ -0,0 +1,27 @@
+package org.formation.sylla.testJPA.stockage;
+
+import java.util.List;
+
+import org.formation.sylla.testJPA.service.PersonnePOJO;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class DaoJPA {
+	private EntityManager em;
+
+	public DaoJPA() {
+		EntityManagerFactory emf = null;
+		emf = Persistence.createEntityManagerFactory("testJPA");
+		em = emf.createEntityManager();
+
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<PersonnePOJO> lireTous(){
+	return em.createQuery("SELECT p FROM PersonnePOJO p").getResultList();
+	}
+
+
+}
